ceae313eebef8128f3d7c94fc5b1e66b293ae486
jian <janine.jian.chen@gmail.com>
2020-02-24 15:17:04 +0800

    create a springboot project

---
+++ src/main/java/org/janine/jian/SpringbootSpikeServerApplication.java
@@ -0,0 +1,13 @@
+package org.janine.jian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpikeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/janine/jian/SpringbootSpikeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.janine.jian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpikeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
